7e3ebac90205ca63aad8a929f86771b70b91c265
Nure Anha Tamanna <nureanha99@gmail.com>
2025-12-10 22:01:15 +0600

    error solve

--- src/Pages/ListingDetails.jsx
+++ src/Pages/ListingDetails.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react';
 import { useParams } from 'react-router';
 import { AuthContext } from './Authentication/Auth/AuthContext';
 import axios from 'axios';
-import { toast, ToastContainer } from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import Swal from 'sweetalert2';
 
 const ListingDetails = () => {
